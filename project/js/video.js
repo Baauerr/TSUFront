45c4c74cdeada431c72b4e21cd0b98546f6dbf20
vardee <118890677+vardee@users.noreply.github.com>
2023-04-22 14:04:27 +0700

    Add files via upload

--- project/js/video.js
+++ project/js/video.js
@@ -1,22 +1,18 @@
-const video = document.getElementById("background-video");
-const button = document.getElementById("play-button");
+const video = document.getElementById('video-background');
+const toggleBtn = document.getElementById('toggle-btn');
+let isPlaying = false;
 
-button.addEventListener("click", function() {
-  if (video.paused) {
-    video.play();
-    button.innerHTML = "Pause Video";
-  } else {
+toggleBtn.addEventListener('click', function() {
+  if (isPlaying) {
     video.pause();
-    button.innerHTML = "Play Video";
+    video.muted = true;
+    video.style.display = 'none';
+    toggleBtn.innerHTML = 'CONTINUE AMAN';
+  } else {
+    video.play();
+    video.muted = false;
+    video.style.display = 'block';
+    toggleBtn.innerHTML = 'PAUSE AMAN';
   }
-});
-
-video.addEventListener("play", function() {
-  document.body.style.background = "#000 url('path/to/video-thumbnail.jpg') no-repeat center center fixed";
-  document.body.style.backgroundSize = "cover";
-});
-
-video.addEventListener("pause", function() {
-  document.body.style.background = "#fff url('path/to/image.jpg') no-repeat center center fixed";
-  document.body.style.backgroundSize = "cover";
+  isPlaying = !isPlaying;
 });
